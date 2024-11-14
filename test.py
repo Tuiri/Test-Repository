86ef487d99a35218669be2bde801f4109982eca4
Tuiri <chrisnyaga01@gmail.com>
2024-11-14 03:07:30 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+# Calculate the square of numbers in a list
+numbers = [1, 2, 3, 4, 5]
+squares = [x**2 for x in numbers]
+print("Original numbers:", numbers)
+print("Squared numbers:", squares)
+print("Sum of squares:", sum(squares))
